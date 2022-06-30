451763f91cd521c9b51e2ff38676e85297f9788d
cnunez-s <cnunez-s@student.42madrid.com>
2022-06-30 20:57:39 +0200

    Tenis

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cnunez-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 16:58:44 by cnunez-s          #+#    #+#             */
-/*   Updated: 2022/06/30 17:36:10 by cnunez-s         ###   ########.fr       */
+/*   Updated: 2022/06/30 20:56:48 by cnunez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,13 @@ int	main (int argc, int argv)
 	stack_a = NULL;
 	stack_b = NULL;
 	tmn = 0;
-
-	while (argc > 1)
+	
+	if (argc > 1)
 	{
 
 	}
 }
+//1º - creamos los dos stacks como listas de las variables
+//2º - iniciamos en NULL y declaramos en caso de ser los argc menor o igual a 1
+//3º - declaramos se inicia que "argc > 1" en caso contrario no entra
+//4º - cuando entra hay que "parsear" lus numeros dados para poder trabajar ()
