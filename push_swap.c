8cc59c9c4dd4a48b03e43065cd87f33d51e43918
cnunez-s <cnunez-s@student.42madrid.com>
2022-07-04 16:55:26 +0200

    Parsel

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,13 @@
 /*   By: cnunez-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 16:58:44 by cnunez-s          #+#    #+#             */
-/*   Updated: 2022/06/30 20:56:48 by cnunez-s         ###   ########.fr       */
+/*   Updated: 2022/07/04 16:52:54 by cnunez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.c"
 
-int	main (int argc, int argv)
+int	main (int argc, int **argv)
 {
 	t_list	stack_a;
 	t_list	stack_b;
@@ -24,10 +24,13 @@ int	main (int argc, int argv)
 	
 	if (argc > 1)
 	{
-
+		ft_parselº:x
+		()
 	}
 }
 //1º - creamos los dos stacks como listas de las variables
 //2º - iniciamos en NULL y declaramos en caso de ser los argc menor o igual a 1
 //3º - declaramos se inicia que "argc > 1" en caso contrario no entra
-//4º - cuando entra hay que "parsear" lus numeros dados para poder trabajar ()
+//4º - cuando entra hay que "parsear" lus numeros dados para poder trabajar
+//     en la funcion de parseo trabajaremos con split y atoi
+//5º - 
