301970043f2739e5e7c77b23c0efbcdde31fce3e
cnunez-s <cnunez-s@student.42madrid.com>
2022-07-06 18:40:55 +0200

    Planing

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,13 @@
 /*   By: cnunez-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 16:58:44 by cnunez-s          #+#    #+#             */
-/*   Updated: 2022/07/06 13:00:52 by cnunez-s         ###   ########.fr       */
+/*   Updated: 2022/07/06 18:40:32 by cnunez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.c"
 
-int	main (int argc, int **argv)
+int	main (int argc, char **argv)
 {
 	t_list	stack_a;
 	t_list	stack_b;
@@ -22,9 +22,11 @@ int	main (int argc, int **argv)
 	stack_b = NULL;
 	tmn = 0;
 	
-	if (argc > 1)
+	if (argc == 1)
+		return (0);
+	if (argc == 2)
 	{
-		ft_parsel()
+
 	}
 }
 //1º - creamos los dos stacks como listas de las variables
@@ -32,9 +34,11 @@ int	main (int argc, int **argv)
 //3º - declaramos se inicia que "argc > 1" en caso contrario no entra
 //4º - cuando entra hay que "parsear" lus numeros dados para poder trabajar
 //     en la funcion de parseo trabajaremos con split y atoi
-//5º - recorremos los argumentos dados y los guardamos en el "stack A2" para poder usarlos
+//5º - recorremos los argumentos dados y los guardamos en el "stack A" para poder usarlos
 //6º - al tener todo en la lista hay que comprobar que no se repiten numeros, en caso de ser
 //     asi el programa debería darnos e imprimir "Error"
 //7º - Seguido abrá que crear una funcion que nos verifique que en "stack A" estan los 
 //     nodos ordenados, en caso de ya ser ordenados no realizar ninguna acción y se termina
-//8º - En caso de no estarlo ordenarlos usando el algritmo de ordenacion deseado
+//8º - En caso de no estarlo ordenarlos usando el algritmo de ordenacion deseado usando las
+//     funciones que nos facilitan en el subject
+//     ¡Merge Sort! - array - 
